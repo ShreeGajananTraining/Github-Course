312c7eaa2c3a9fb4a3001fa58ddb0bf34f9f48c9
ShreeGajananTraining <mantri4.poonam@gmail.com>
2025-11-09 12:15:37 +0530

    added

--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -3,8 +3,14 @@ package pages;
 public class SearchPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 System.out.println("Hello");
+
+String un ="admin";
+String pwd ="admin";
+
+System.out.println("loged");
+
 	}
 
 }
