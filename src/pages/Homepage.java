8cc8872d96090ce0f387e305323330ee6c2ba772
ShreeGajananTraining <mantri4.poonam@gmail.com>
2025-11-09 10:50:44 +0530

    add github project

---
+++ src/pages/Homepage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class Homepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/test/Loginpagetest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Loginpagetest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+
+}
